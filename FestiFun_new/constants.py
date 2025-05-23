da859ec53cbe140dbc5955a1a6672466c6bbd926
Anastaciass <anastassijamalkova91@gmail.com>
2025-05-23 09:26:15 +0200

    Update constants.py

--- FestiFun_new/constants.py
+++ FestiFun_new/constants.py
@@ -1,2 +1,2 @@
 sender_email = "costindylan@gmail.com"  # Replace with your email address
-password = "cntx rotx iegc azne"  # Replace with your App Password
+password = "uiig vpqo mdib wdjg"  # Replace with your App Password
